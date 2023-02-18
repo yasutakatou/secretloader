c79876e22dcd9995899ae8546007ffb6cc61febe
yasutakatou <fzk01325@nifty.com>
2023-02-18 15:05:52 +0900

    Add files via upload

--- secretloader.go
+++ secretloader.go
@@ -1,74 +1,98 @@
+/*
+ * A tool that leverages AWS Secrets Manager to create a config file.
+ *
+ * @author    yasutakatou
+ * @copyright 2023 yasutakatou
+ * @license   xxx
+ */
 package main
 
 import (
 	"bufio"
+	"context"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
-)
+	"time"
 
-var (
-	secretName   string = ""
-	region       string = "ap-northeast-1"
-	versionStage string = "AWSCURRENT"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
-
-https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html
 
 func main() {
-	// svc := secretsmanager.New(
-	// 	session.New(),
-	// 	aws.NewConfig().WithRegion(region),
-	// )
+	_secretStr := flag.String("secretStr", "{}", "[-secretStr=Symbol to define the secret name. ex. []")
+	_onlyOnce := flag.Bool("onlyOnce", true, "[-onlyOnce=Non-loop execution mode. (true is enable)]")
+	_loopDuration := flag.Int("loopDuration", 1440, "[-loopDuration=Interval at which to execute the loop. (default is 1 day = 1440 minutes)]")
+	_inputFile := flag.String("inputFile", "template.ini", "[-inputFile=Input file name and its path.")
+	//_outputFile := flag.String("outputFile", "config.ini", "[-outputFile=Output file name and its path.")
 
-	// input := &secretsmanager.GetSecretValueInput{
-	// 	SecretId:     aws.String(secretName),
-	// 	VersionStage: aws.String(versionStage),
-	// }
+	flag.Parse()
 
-	// result, err := svc.GetSecretValue(input)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// var secretString string = *result.SecretString
+	secretStr := string(*_secretStr)
 
-	// fmt.Println(secretString)
-
-	name := "sample.txt"
-
-	f, _ := os.Open(name)
-	bu := bufio.NewReaderSize(f, 1024)
+	//file, err := os.Create(*_outputFile)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer file.Close()
 
 	for {
-		line, _, err := bu.ReadLine()
-		if err == io.EOF {
+		f, _ := os.Open(*_inputFile)
+		bu := bufio.NewReaderSize(f, 1024)
+
+		for {
+			line, _, err := bu.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("%s\n", line)
+			fmt.Println(line[0])
+			fmt.Println(line[len(line)-1])
+
+			if secretStr[0] == line[0] && secretStr[len(secretStr)-1] == line[len(line)-1] {
+				secretName := string(string(line[1 : len(line)-1]))
+				fmt.Println(secretName)
+				readSecret(secretName)
+			}
+
+			//_, err = file.WriteString(string(line) + "\n")
+			//if err != nil {
+			//	log.Fatal(err)
+			//}
+		}
+		f.Close()
+
+		if *_onlyOnce == true {
 			break
 		}
-		fmt.Printf("%s\n", line)
+
+		time.Sleep(time.Minute * time.Duration(*_loopDuration))
 	}
+	os.Exit(0)
 }
 
-/*
--secretStr={}
--onlyOnce
--loop=30
+func readSecret(secretName string) {
+	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-[ALERT]
-{SECRET1}
-[ALLOWID]
-{SECRET2}
-{SECRET3}
-[REJECT]
-rejectrule1	escalation1	rm	passwd	vi
-[HOSTS]
-hostlabel1	pi1	192.168.0.1	22	pi1	myPassword1	/bin/bash
-hostlabel2	pi2	192.168.0.2	22	pi2	myPassword2	/bin/ash
-[USERS]
-U024ZT3BHU5	~/	0
-[ALLOW]
-allowrule1	escalation1	cd	ls	cat	ps	df	find
-[ADMINS]
-admin
-[REPORT]
-C0256BTKP54
-*/
+	// Create Secrets Manager client
+	svc := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		panic(err.Error())
+	}
+	var secretString string = *result.SecretString
+
+	fmt.Println(secretString)
+}
